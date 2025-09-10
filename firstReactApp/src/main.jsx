58566ad9e131ccd9103187df4c5c86336bf5e8f7
Rong Jun <choo.rongjun@gmail.com>
2025-09-10 10:41:06 +0800

    Week 5 even more stuff

--- firstReactApp/src/main.jsx
+++ firstReactApp/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import FormValidationExample from './FormValidationExample.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <FormValidationExample/>
   </StrictMode>,
 )
