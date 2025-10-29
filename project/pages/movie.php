054636c13deb1d879c5cf616be2853958c84e358
Avi <gibraltar.av@gmail.com>
2025-10-29 11:22:19 +0800

    Week 11 Updates

--- project/pages/movie.php
+++ project/pages/movie.php
@@ -3,12 +3,20 @@
 include './pages/movie.css'; 
 
 $current_date = date("Y-m-d");
+
+if (isset($_GET['movie_id'])) {
+    $movie_id = $_GET['movie_id'];
+} else {
+    header("Location: index.php?page=404");
+    exit();
+}
+
 $sql = "SELECT m.*,s.schedule_id,s.date FROM movies m INNER JOIN schedules s ON m.movie_id=s.movie_id WHERE m.movie_id=?;";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $_GET['movie_id']);
+$stmt->bind_param("i", $movie_id);
 $stmt->execute();
 $result = $stmt->get_result();
-$first_row = $result->fetch_assoc();
+$row = $result->fetch_assoc();
 ?>
 </style>
 
@@ -18,25 +26,27 @@ $first_row = $result->fetch_assoc();
     <input type="submit" name="search-btn" id="search-btn">
 </section>
 
-<h1><?= $first_row['name'] ?></h1>
+<h1><?= $row['name'] ?></h1>
 
 <section class="movie-information">
-    <img src="./images/<?= $first_row['movie_poster'] ?>" alt="Movie Poster Here" class="movie-poster">
+    <img src="./images/<?= $row['movie_poster'] ?>" alt="Movie Poster Here" class="movie-poster">
     <div class="movie-description">
         <div>
             <h2>Description: </h2>
-            <h3><?= $first_row['description'] ?></h3>
+            <h3><?= $row['description'] ?></h3>
         </div>
         <div>
             <h2>Showtimes:</h2>
-            <h3><?= $first_row['date'] ?></h3>
-            <?php
-            while ($row = $result->fetch_assoc()) {
-            ?> 
-            <h3><?= $row['date'] ?></h3>
-            <?php
-            }
-            ?>
+            <div class="showtimes">
+                <?php
+                do {
+                    $date = date("d-m-Y\nh:i:s a",strtotime($row['date']));
+                ?>
+                <a href="index.php?page=seats&schedule_id=<?=$row['schedule_id']?>"><?= $date ?></a>
+                <?php
+                } while ($row = $result->fetch_assoc());
+                ?>
+            </div>
         </div>
     </div>
 </section>
